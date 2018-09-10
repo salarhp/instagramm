422d206671d8ce5b81badbe8175f93f69f445b7b
salarhp <salarhoseinpuor@gmail.com>
2018-09-10 16:33:31 +0430

    add pics actions view

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,29 @@
 class PicsController < ApplicationController
-def index
 
-end
+    def index
+
+    end
+
+    def new
+        @pic = Pic.new
+    end
+
+    def create
+        @pic = Pic.new(pic_params)
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def pic_params
+        params.require(:pic).permit(:title,:description)
+    end
+
 
 end
