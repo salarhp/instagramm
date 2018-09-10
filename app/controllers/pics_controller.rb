d9b16e2d0d83b0e5848408dccf350d71c5f21432
salarhp <salarhoseinpuor@gmail.com>
2018-09-10 17:23:56 +0430

    add Edit update and destroy actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,7 @@
 class PicsController < ApplicationController
     before_action :find_pic, only: [:show, :edit, :update, :destroy]
     def index
-
+        @pics = Pic.all.order("created_at DESC")
     end
 
     def show
@@ -22,11 +22,16 @@ class PicsController < ApplicationController
     end
 
     def update
-
+        if @pic.update(pic_params)
+            redirect_to @pic, notice: "congrate! pic was updateed!"
+        else
+            render 'new'
+        end
     end
 
     def destroy
-
+        @pic.destroy
+        redirect_to root_path
     end
 
     private
